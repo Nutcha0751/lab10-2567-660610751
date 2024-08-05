f8385ee9ef6ff350bfd9bb433c678d8087158550
Nutcha0751 <nutcha_kham@cmu.ac.th>
2024-08-06 00:36:39 +0700

    DL comment

--- src/libs/cleanUser.ts
+++ src/libs/cleanUser.ts
@@ -9,16 +9,3 @@ export const cleanUser = (user: any) => {
   };
   return newUser;
 };
-
-/*import { UserCardProps } from "./types";
-
-export const cleanUser = (user: any): UserCardProps => {
-  const newUser: UserCardProps = {
-    name: `${user?.name?.first ?? ''} ${user?.name?.last ?? ''}`,
-    email: user?.email ?? '',
-    imgUrl: user?.picture?.large ?? '',
-    address: `${user?.location?.city ?? ''} ${user?.location?.state ?? ''}`,
-  };
-
-  return newUser;
-};*/
